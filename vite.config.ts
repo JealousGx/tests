48c135fef3738fb7e62f81fb01c83c31af46dd1e
Abdul Mateen <59867217+JealousGx@users.noreply.github.com>
2026-03-28 09:50:48 +0500

    chore: add Netlify Vite plugin and update dependencies
    - Added @netlify/vite-plugin-tanstack-start to devDependencies in package.json.
    - Updated vite.config.ts to include the Netlify plugin in the Vite configuration.
    - Reformatted import statements for consistency.

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,23 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { devtools } from "@tanstack/devtools-vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import netlify from "@netlify/vite-plugin-tanstack-start";
+
+import tailwindcss from "@tailwindcss/vite";
+import viteReact from "@vitejs/plugin-react";
 
 const config = defineConfig({
   plugins: [
     devtools(),
-    tsconfigPaths({ projects: ['./tsconfig.json'] }),
+    tsconfigPaths({ projects: ["./tsconfig.json"] }),
     tailwindcss(),
     tanstackStart(),
+    netlify(),
     viteReact(),
   ],
-})
+});
 
-export default config
+export default config;
