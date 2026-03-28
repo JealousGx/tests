38ebb37e0d52686fed5b53f113fc9ffb9c2c0089
Abdul Mateen <59867217+JealousGx@users.noreply.github.com>
2026-03-28 09:46:19 +0500

    Fix title in root route and reorder import statements

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,6 @@
+import { TanStackDevtools } from '@tanstack/react-devtools'
 import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 
@@ -17,7 +17,7 @@ export const Route = createRootRoute({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'Job Board UI',
+        title: 'Job Board',
       },
     ],
     links: [
